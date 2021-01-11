26af591e27fab86e8bcdb6d58e026d500f6c3d1d
Galapriyank <gala.pr@husky.neu.edu>
2021-01-11 09:01:35 -0500

    Sidebar component added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     //BEM naming convention
     <div className="App">
-      <h1>Hello World!!</h1>
+      {/* <h1>Hello World!!</h1> */}
       <Header />
 
       <div className="app__body">
